5ef1c41f141f44556a4bb246bd57f5e251e1b48d
Dadan Julianto <2400016070@webmail.uad.ac.id>
2025-11-12 15:09:41 +0700

    2400016070_tekwebprak5

---
+++ Tugas praktikum 5 (figma)/script.js
@@ -0,0 +1,7 @@
+// Hamburger menu functionality
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
