ad18d69f1aea3dc8594649b75d9cc9bf5f2727c5
Dadan Julianto <2400016070@webmail.uad.ac.id>
2025-12-11 22:55:31 +0700

    add tugas praktikum 7

--- Tugas praktikum 7/vite.config.js
+++ Tugas praktikum 7/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/2400016070_Dadan-Julianto_A/',
+  base: '/DadanJulianto_A/',
   plugins: [react()],
 })
